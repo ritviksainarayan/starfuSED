62fb41b3c69cf3ebc32f5a25b415d09b0bd018ea
Ritvik Sai Narayan <100477022+ritviksainarayan@users.noreply.github.com>
2026-01-11 14:44:14 -0600

    Implement dust correction in Photometry class
    Added dust correction method to Photometry class.

--- starfused/preprocess/.py
+++ starfused/preprocess/.py
@@ -1,4 +1,3 @@
-#%%
 """
     Preprocess photometric data from the Vizier SED service.
 """
@@ -9,9 +8,24 @@ from astropy.table import Table
 import astropy.units as u
 from astropy.constants import c
 from astropy.coordinates import SkyCoord
+from astroquery.ipac.irsa.irsa_dust import IrsaDust
 import requests
+from astropy.io import fits
+from io import BytesIO
+from scipy.interpolate import interp1d
 
-class PhotometricQuery:
+EXTINCTION_CURVES = {
+    'mw'        : 'milkyway_diffuse_001.fits',
+    'milky_way' : 'milkyway_diffuse_001.fits',
+    'mw_dense'  : 'milkyway_dense_001.fits',
+    'lmc'       : 'lmc_diffuse_001.fits',
+    'lmc_30dor' : 'lmc_30dorshell_001.fits',
+    'smc_bar'   : 'smc_bar_001.fits',
+}
+
+class Photometry:
+
+
 
     def query(name=None, ra=None, dec=None, filters=['GALEX','UVOT','SDSS','Gaia','2MASS','WISE'], radius=1):
         """
@@ -70,7 +84,63 @@ class PhotometricQuery:
         
         return df
     
+    def dust_correction(df, extinction='mw', rv=3.1, dustmap='SandF'):
+        """
+        Apply dust extinction correction to photometric data. 
+        
+        Parameters
+        ----------
+        df : pandas.DataFrame
+            DataFrame containing photometric data with columns 'sed_wave', 'sed_flux', 'sed_eflux', '_RAJ2000', '_DEJ2000'.
+        extinction : str, optional
+            Extinction curve to use. Options are 'mw' or 'milky_way', 'mw_dense', 'lmc', 'lmc_30dor', 'smc_bar'.
+        dustmap : str, optional
+            Dust map to use for E(B-V) retrieval. Options are 'SFD' or 'SandF'.
+
+        Returns
+        -------
+        pandas.DataFrame 
+            DataFrame with dust-corrected 'sed_flux' and 'sed_eflux' columns.
+        """
+
+        ra = df['_RAJ2000'].values[0]
+        dec = df['_DEJ2000'].values[0]
+
+        # query coordinate
+        coord = SkyCoord(ra=ra*u.degree, dec=dec*u.degree, frame='icrs', equinox='J2000.0')
+
+        dusttable = IrsaDust.get_query_table(coord, radius=2*u.deg, section='ebv')
+        
+        if dustmap.upper() == 'SFD':
+            ebv = dusttable['ext SFD mean'][0]
+        elif dustmap.upper() == 'SANDF':
+            ebv = dusttable['ext SandF mean'][0]
+        else:
+            raise ValueError("dustmap must be either 'SFD' or 'SandF'")
+
+        print(f"Retrived E(B-V) = {ebv:.3f} from {dustmap} dust map.")
+        
+        if extinction.lower() not in EXTINCTION_CURVES.keys():
+            raise ValueError(f"Extinction curve '{extinction}' not recognized. Available options are: {list(EXTINCTION_CURVES.keys())}")
+        
+        url = f"https://archive.stsci.edu/hlsps/reference-atlases/cdbs/extinction/{EXTINCTION_CURVES[extinction.lower()]}"
+        
+        response = requests.get(url)
+
+        fitsdata = fits.open(BytesIO(response.content))
+
+        fitswave = (1 / fitsdata[1].data['WAVELENGTH']) * 1e4 # in angstroms
+        fits_av_ebv = fitsdata[1].data['Av/E(B-V)']
+
+        av_ebv_obs = interp1d(fitswave, fits_av_ebv, kind='linear', bounds_error=False, fill_value="extrapolate")(df['sed_wave'].values)
+        a_lambda = ebv * av_ebv_obs
+
+        # correction factor = 10**(0.4 * A_lambda)
+
+        df_corrected = df.copy()
+        df_corrected['sed_flux']  *= 10**(0.4 * a_lambda)
+        df_corrected['sed_eflux'] *= 10**(0.4 * a_lambda)
+
+        return df_corrected
 
 
-
-# %%
